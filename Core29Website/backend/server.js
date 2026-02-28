b7007a38af67ce799f83759d9f8eedc4932888df
OlegKov33 <olkova.oleg.kovalchuk@gmail.com>
2026-02-28 14:29:00 +0000

    server connected

--- Core29Website/backend/server.js
+++ Core29Website/backend/server.js
@@ -1,5 +1,20 @@
-let http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(8080);
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 8080;
+
+// Absolute path to frontend/pages
+const frontendPath = path.join(__dirname, '../frontend/pages');
+
+// Serve static files (CSS, JS, images inside pages folder)
+app.use(express.static(frontendPath));
+
+// Route for index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(frontendPath, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
+});
